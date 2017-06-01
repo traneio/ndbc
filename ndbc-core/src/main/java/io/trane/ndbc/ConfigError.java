b4371bd1d9844f920f6a3f944d89f16fed18cc44
fwbrasil <fwbrasil@gmail.com>
2017-05-31 22:44:23 -0700

    minor changes

--- ndbc-core/src/main/java/io/trane/ndbc/ConfigError.java
+++ ndbc-core/src/main/java/io/trane/ndbc/ConfigError.java
@@ -6,33 +6,17 @@ import java.util.Properties;
 public class ConfigError extends RuntimeException {
   private static final long serialVersionUID = -5455508571025077170L;
 
-  private final Properties properties;
-  private final String prefix;
-  private final String name;
-  private final Optional<Exception> cause;
+  final Properties properties;
+  final String prefix;
+  final String name;
+  final Optional<Exception> cause;
 
   public ConfigError(Properties properties, String prefix, String name, Optional<Exception> cause) {
-    super("");
+    super("Invalid config `" + prefix + "." + name + "`." + cause.map(e -> "Cause: " + e.toString()).orElse(""));
     this.properties = properties;
     this.prefix = prefix;
     this.name = name;
     this.cause = cause;
-  }
-
-  public Properties getProperties() {
-    return properties;
-  }
-
-  public String getPrefix() {
-    return prefix;
-  }
-
-  public String getName() {
-    return name;
-  }
-  
-  @Override
-  public String toString() {
-    return "Invalid config `" + prefix + "." + name + "`." + cause.map(e -> "Cause: " + e.toString()).orElse("");
+    cause.map(this::initCause);
   }
 }
