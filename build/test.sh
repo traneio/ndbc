ddac77a4265c3a83a3e4a0137a24c4f2bc65228c
fwbrasil <fwbrasil@gmail.com>
2018-07-31 13:08:20 -0300

    trying to fix build

--- build/test.sh
+++ build/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately
 
+mvn install
 mvn org.jacoco:jacoco-maven-plugin:prepare-agent -pl $PROJECTS test
